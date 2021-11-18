e084451c4a59f86820ca7c479eccf461ea3d444f
Eduard <alehined@mail.ru>
2021-11-19 00:50:29 +0300

    add lesson-1

---
+++ загот/dist/index.js
@@ -0,0 +1,25 @@
+class Book {
+    constructor(name, genre, pageAmount) {
+        this.name = name;
+        this.genre = genre;
+        this.pageAmount = pageAmount;
+    }
+}
+const books = [
+    new Book('Harry Potter', 'fantasy', 980),
+    new Book('Lord of the Ring', 'fantasy', 1001),
+    new Book('How to be productive', 'lifestyle', 500),
+    new Book('Game of trones', 'fantasy', 999)
+];
+function findSuitableBook(genre, pagesLimit, multipleRecommendations = true) {
+    const findAlgorithm = (book) => {
+        return book.genre === genre && book.pageAmount <= pagesLimit;
+    };
+    if (multipleRecommendations) {
+        return books.filter(findAlgorithm);
+    }
+    else {
+        return books.find(findAlgorithm);
+    }
+}
+console.log(findSuitableBook('fantasy', 1000));
---
+++ загот/src/book-collection.ts
@@ -0,0 +1,8 @@
+import { Book } from './book.js'
+
+export const books: Book[] = [
+  new Book('Harry Potter', 'fantasy', 980),
+  new Book('Lord of the Ring', 'fantasy', 1001),
+  new Book('How to be productive', 'lifestyle', 500),
+  new Book('Game of trones', 'fantasy', 999)
+]
