88f580497d7948d4179d896ceecdfcd02ed921f4
Eduard-al <75543603+Eduard-al@users.noreply.github.com>
2021-11-25 00:57:26 +0300

    Delete загот directory

--- загот/src/book-collection.ts
+++
@@ -1,8 +0,0 @@
-import { Book } from './book.js'
-
-export const books: Book[] = [
-  new Book('Harry Potter', 'fantasy', 980),
-  new Book('Lord of the Ring', 'fantasy', 1001),
-  new Book('How to be productive', 'lifestyle', 500),
-  new Book('Game of trones', 'fantasy', 999)
-]
